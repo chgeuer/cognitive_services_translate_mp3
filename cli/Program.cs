69e062f382e9b3650983a689da84c1b9bf71b577
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2019-11-24 19:54:03 +0100

    WAV-to-MP3 implemented

--- cli/Program.cs
+++ cli/Program.cs
@@ -17,11 +17,15 @@
 
             var subscriptionKey = Environment.GetEnvironmentVariable("SPEECH_API_KEY");
             var region = Environment.GetEnvironmentVariable("SPEECH_API_REGION");
-            
+
             using var mp3stream = File.OpenRead(inputFilename);
 
             await CognitiveExtensions.Translate(mp3stream, subscriptionKey, region,
                 outputFilename, Mp3ConversionAlgorithm.FFMPEG);
+
+            //using var wavStream = File.OpenRead(fullPath("translated.wav"));
+            //var mp3bytes = await wavStream.ConvertWAV2MP3_NAudio();
+            //await File.WriteAllBytesAsync(fullPath("translated.mp3"), mp3bytes);
         }
     }
 }
--- lib/SoundExtensions.cs
+++ lib/SoundExtensions.cs
@@ -3,12 +3,13 @@
     using System;
     using System.IO;
     using System.Threading.Tasks;
+    using NAudio.MediaFoundation;
     using NAudio.Wave;
 
     public enum Mp3ConversionAlgorithm
     {
-        NAudio,
-        FFMPEG
+        NAudio = 0,
+        FFMPEG = 1
     }
 
     public static class SoundExtensions
@@ -16,6 +17,7 @@
         const int audio_sample_rate = 16000;
         const int audio_bit_per_sample = 16;
         const int audio_channels = 1;
+        const int desiredMP3bitRate = 48000;
 
         public static Task<byte[]> ConvertMP3(this Stream mp3Stream, Mp3ConversionAlgorithm algo)
         {
@@ -41,6 +43,37 @@
             return wavStream.ToArray();
         }
 
+        public static async Task<byte[]> ConvertWAV2MP3_NAudio(this Stream wavStream)
+        {
+            var tmpFile = Path.GetTempFileName() + ".mp3";
+            MediaFoundationApi.Startup();
+            try
+            {
+                using var reader = new WaveFileReader(inputStream: wavStream);
+
+                //MediaFoundationEncoder.EncodeToMp3(reader, outputFile: tmpFile, desiredBitRate: desiredMP3bitRate);
+
+                var mediaType = MediaFoundationEncoder.SelectMediaType(
+                    AudioSubtypes.MFAudioFormat_MP3, 
+                    new WaveFormat(sampleRate: audio_sample_rate, channels: audio_channels), 
+                    desiredBitRate: desiredMP3bitRate);
+                if (mediaType == null)
+                {
+                    throw new NotSupportedException();
+                }
+                using var enc = new MediaFoundationEncoder(mediaType);
+                enc.Encode(outputFile: tmpFile, reader);
+
+                var result = await File.ReadAllBytesAsync(tmpFile);
+                return result;
+            }
+            finally
+            {
+                MediaFoundationApi.Shutdown();
+                try { File.Delete(tmpFile); } catch (Exception) { }
+            }
+        }
+
         private static async Task<byte[]> ConvertMP3_FFMPEG(this Stream mp3Stream)
         {
             var audio_codec = audio_bit_per_sample switch
@@ -58,7 +91,21 @@
             string ffmpeg_args = $"{readFromStdin} {produceWAV} {audioParams} {writeToStdout}";
 
             var result = await ProcessExtensions.RunAsync("ffmpeg", ffmpeg_args, mp3Stream);
-            return result.Stdout.TweakWavLength();
+            var log = System.Text.Encoding.UTF8.GetString(result.Stderr);
+
+            return result.Stdout; // .TweakWavLength();
+        }
+
+        private static async Task<byte[]> ConvertWAV2MP3_FFMPEG(this Stream stream)
+        {
+            var audio_codec = "mp3";
+            string readFromStdin = "-i -";
+            string produceMP3 = $"-c:a {audio_codec}";
+            string writeToStdout = "-";
+            string ffmpeg_args = $"{readFromStdin} {produceMP3} {writeToStdout}";
+            var result = await ProcessExtensions.RunAsync("ffmpeg", ffmpeg_args, stream);
+            var log = System.Text.Encoding.UTF8.GetString(result.Stderr);
+            return result.Stdout;
         }
 
         private static byte[] CloneArray(this byte[] array)
@@ -68,29 +115,27 @@
             return result;
         }
 
-        private static byte[] TweakWavLength(this byte[] wavByteArray)
-        {
-            // un-fuck the WAV length...
-            // mplayer.exe -demuxer rawaudio -rawaudio rate=16000:channels=1:samplesize=2 -ao pcm out.wav
-            var bytes = wavByteArray.CloneArray();
-            var byteLength = bytes.Length;
-            void tweak(int index, int rotate, long subtractionOffset)
-            {
-                var length = byteLength - subtractionOffset;
-                bytes[index] = (byte)((length >> (rotate * 8)) & 0xff);
-            }
-            void patchLong(int startIndex, long subtractionOffset)
-            {
-                for (var i = 0; i < 4; i++)
-                {
-                    tweak(startIndex + i, i, subtractionOffset);
-                }
-            }
-
-            patchLong(0x04, 8);
-            patchLong(0x74, 120);
-
-            return bytes;
-        }
+        //private static byte[] TweakWavLength(this byte[] wavByteArray)
+        //{
+        //    // un-fuck the WAV length...
+        //    // mplayer.exe -demuxer rawaudio -rawaudio rate=16000:channels=1:samplesize=2 -ao pcm out.wav
+        //    var bytes = wavByteArray.CloneArray();
+        //    var byteLength = bytes.Length;
+        //    void tweak(int index, int rotate, long subtractionOffset)
+        //    {
+        //        var length = byteLength - subtractionOffset;
+        //        bytes[index] = (byte)((length >> (rotate * 8)) & 0xff);
+        //    }
+        //    void patchLong(int startIndex, long subtractionOffset)
+        //    {
+        //        for (var i = 0; i < 4; i++)
+        //        {
+        //            tweak(startIndex + i, i, subtractionOffset);
+        //        }
+        //    }
+        //    patchLong(0x04, 8);
+        //    patchLong(0x74, 120);
+        //    return bytes;
+        //}
     }
 }
